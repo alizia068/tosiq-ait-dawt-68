bd957d23fadef4cf9d65be4d7e4c44c068b270ae
Muhammad Talha <mtalha0018@gmail.com>
2025-08-29 19:45:19 +0500

    JS Basic, If else, js bill assignment, BS template design assignment(5 sep - Friday)

---
+++ web/javascript/main.js
@@ -0,0 +1,75 @@
+//***** Data types *****/
+// number: 1234, 2.3, 55.67, 23488880000000
+// string: "This is my Homeland"
+// array : myarray = ["Apple", "Orange", "Bnana"] // myarray[1]
+// object: user= { name: "Asad", email: "abc@gmail.com"} // user.name
+// document.getElementById("box").innerText = "This is my Homeland";
+// var text = "This is JavaSCRIPT";
+// console.log(text);
+// alert(text);
+// var a = 10;
+// var b = 30;
+// var sum = a+b;
+// var x = 5;
+// var y = 10;
+// // x + y + 3 + (x+y)
+// document.getElementById("box").innerText = x + y + 3 + (x+y)
+
+// ***************************
+// Assignment: calculate electricity bill
+// Requirements: 1- 200, 2- 250, 3- 300 and above (else if (unit > 300))
+function calcBill(){
+    var unit = 244
+    var unitPrice = 14;
+    var tax = 355;
+    var fuelTax = 298;
+    if (unit <= 200){
+        output.innerText = unit * unitPrice + tax + fuelTax;
+    } else if (unit > 200 && unit <= 230) { // defined range
+        var unitPrice = 28
+        output.innerText = unit * unitPrice + tax + fuelTax;
+    }
+}
+// write a JS program that tells wheater number is EVEN or ODD
+function getResult() {
+  var no = document.getElementById("no").value;
+  var output = document.getElementById("output");
+  // aert(no);
+  if (no > 0) {
+    // if TRUE
+    output.innerText = "The given number is positive";
+  } else {
+    // if FALSE
+    output.innerText = "The given number is negative";
+  }
+}
+// find day
+function getDay() {
+  var no = document.getElementById("no-day").value;
+  var output = document.getElementById("day-output");
+  //   alert(no);
+  if (no == 1) {
+    // if TRUE
+    output.innerText = "The day is Monday";
+  } else if (no == 2) {
+    // if TRUE
+    output.innerText = "The day is Tuesday";
+  } else if (no == 3) {
+    // if TRUE
+    output.innerText = "The day is Wednesday";
+  } else if (no == 4) {
+    // if TRUE
+    output.innerText = "The day is Thursday";
+  } else if (no == 5) {
+    // if TRUE
+    output.innerText = "The day is Friday";
+  } else if (no == 6) {
+    // if TRUE
+    output.innerText = "The day is Saturday";
+  } else if (no == 7) {
+    // if TRUE
+    output.innerText = "The day is Sunday";
+  } else {
+    output.innerText = "The given is Invalid!";
+  }
+}
