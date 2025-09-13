1317b32d03702caba952c7feee155f376adc62b7
Muhammad Talha <mtalha0018@gmail.com>
2025-09-13 18:48:39 +0500

    filter, map js

---
+++ web/assets/js/users-list.js
@@ -0,0 +1,21 @@
+const users = [
+    {id: 1, name: "Samith", email: "Samith@gmail.com", accountStatus: "Approved"},
+    {id: 2, name: "Sam", email: "Sam@gmail.com", accountStatus: "Not Approved"},
+    {id: 3, name: "Alice", email: "Alice@gmail.com", accountStatus: "Approved"},
+    {id: 4, name: "John Doe", email: "John@gmail.com", accountStatus: "Not Approved"},
+    {id: 5, name: "Victoria", email: "Victoria@gmail.com", accountStatus: "Approved"},
+    {id: 6, name: "Jane", email: "Jane@gmail.com", accountStatus: "Not Approved"},
+    {id: 7, name: "Vector", email: "Vector@gmail.com", accountStatus: "Approved"},
+];
+
+let accountStatus = "Not Approved";
+let filteredUsers = users.filter((users)=>{
+    if (users.accountStatus == accountStatus) {
+        return users;
+    };
+});
+console.log(filteredUsers);
+
+// users.map((user) => {
+//     console.log(user.accountStatus);
+// })
