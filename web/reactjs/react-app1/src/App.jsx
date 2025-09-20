db6cedc05723f71c8aaa11bc2a231b743d7531c4
Muhammad Talha <mtalha0018@gmail.com>
2025-09-20 19:39:51 +0500

    ReactJs: react-app1

---
+++ web/reactjs/react-app1/src/App.jsx
@@ -0,0 +1,76 @@
+const heading = {
+      color: "red", 
+      backgroundColor: "yellow"
+    };
+const App = () => {
+  return (
+    <>
+    {/* <h1 style={heading}>This is my heading 1</h1>
+    <h1 className="heading">This is my heading 2</h1> */}
+    <center>
+      <h1>Micheal Harris</h1>
+      <h4>UI/UX Designer | 3D Animation | Software Engineer</h4>
+      <p>Lahore, Pakistan | abc@mail.com | +92 312 34 56 789 | linkedin.com/in/acountname123</p>
+    </center>
+
+    <h1>Summary  / About</h1>
+    <hr />
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo quod quam aperiam suscipit nam vel ut facere magni, culpa in? Soluta voluptatum, porro nemo quaerat incidunt labore dolorum odio optio?
+    Quos eveniet ducimus quis, error possimus non natus cupiditate fugiat, adipisci obcaecati ullam quibusdam dolorum excepturi, inventore tenetur ipsum. Recusandae iure dicta porro totam, aliquid facere nemo minus harum perspiciatis?
+    </p>
+    <h1>Work Experience</h1>
+    <hr />
+    <label>Web Developer</label>
+    <table width="100%">
+      <tr> <td>Islamabad, Pakistan</td> <td align="right">January 2023 &mdash; September 2025</td> </tr>
+    </table>
+    <ul>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, optio facere. Nulla consectetur temporibus dicta sed? Aliquam sit nesciunt expedita voluptatem.Doloribus quidem illo veniam adipisci quam voluptatum laborum facilis fugit nesciunt quia praesentium fuga! Eaque tenetur eos quibusdam.
+      </li>
+      <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ullam omnis sequi consectetur tempore quo earum ad, natus similique in officiis iste praesentium perspiciatis nihil veritatis voluptatum, odit harum eveniet!
+        Tempora nihil nemo, explicabo praesentium tenetur provident repellat pariatur ex atque illo soluta inventore quibusdam a architecto officiis nobis nam non libero doloremque facere officia reiciendis? Tempore hic a suscipit.
+      </li>
+    </ul>
+
+    <label>Web Designer</label>
+    <table width="100%">
+      <tr> <td>Lahore, Pakistan</td> <td align="right"> June 2020 &mdash; September 2023</td> </tr>
+    </table>
+    <ul>
+      <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus, optio facere. Nulla consectetur temporibus dicta sed? Aliquam sit nesciunt expedita voluptatem.Doloribus quidem illo veniam adipisci quam voluptatum laborum facilis fugit nesciunt quia praesentium fuga! Eaque tenetur eos quibusdam.
+      </li>
+      <li>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus ullam omnis sequi consectetur tempore quo earum ad, natus similique in officiis iste praesentium perspiciatis nihil veritatis voluptatum, odit harum eveniet!
+        Tempora nihil nemo, explicabo praesentium tenetur provident repellat pariatur ex atque illo soluta inventore quibusdam a architecto officiis nobis nam non libero doloremque facere officia reiciendis? Tempore hic a suscipit.
+      </li>
+    </ul>
+
+    <h1>Education</h1>
+    <hr />
+    <label>BSCS</label>
+    <table width="100%">
+      <tr> <td>Lahore, Pakistan</td> <td align="right"> Graduation: 2020</td> </tr>
+    </table>
+
+    <h1>Skills</h1>
+    <hr />
+    <ul>
+      <li>Frontend: HTML, CSS, Bootstrap, Javascript, Jquery, React</li>
+      <li>Backend: NodeJS, ExpressJS, MangoDB</li>
+    </ul>
+
+    <h1>Certifications</h1>
+    <hr />
+    <ul>
+      <li>MS-OFFICE</li>
+      <li>Amazon</li>
+      <li>Shopify</li>
+      <li>MERN Stack</li>
+    </ul>
+    </>
+  )
+}
+
+export default App
