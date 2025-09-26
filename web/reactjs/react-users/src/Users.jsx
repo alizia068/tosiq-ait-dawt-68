d35844c70be1109e204aaee6aab9da7b006095e1
Muhammad Talha <mtalha0018@gmail.com>
2025-09-26 20:01:58 +0500

    React js useRef

--- web/reactjs/react-users/src/Users.jsx
+++ web/reactjs/react-users/src/Users.jsx
@@ -1,9 +1,46 @@
+import { useRef } from "react";
 import UserCard from "./components/UserCard";
 import userList from "./utils/userList";
 const Users = () => {
+  const text1 = useRef(null); // React feature: Hooks
+  const text2 = useRef(null); // React feature: Hooks
+
+  const show = () => {
+    // let txt1 = document.getElementById("text").value;
+    let txt1 = text1.current.value;
+    // let txt2 = text2.current.value;
+    text2.current.value = txt1;
+    // let sum = parseInt(txt1) + parseInt(txt2)
+    // console.log((txt1+txt2));
+  };
+
   return (
     <>
       <div className="m-4 text-3xl">User Data</div>
+      <div className="m-4">
+        <input
+          ref={text1}
+          className="border-b-2 p-3 mr-3 focus:outline-0"
+          type="number"
+          placeholder="Type value 1..."
+        />
+
+        <button
+          onClick={show}
+          className="mx-3 bg-amber-200 p-3 rounded shadow cursor-pointer"
+        >
+          Click
+        </button>
+        
+        <input
+          ref={text2}
+          className="border-b-2 p-3 focus:outline-0"
+          type="number"
+          placeholder="Type value 2..."
+        />
+
+        
+      </div>
       {userList.length > 0 ? (
         <div className="m-4 grid grid-cols-12">
           {userList.map((user) => {
--- web/reactjs/react-users/src/utils/userList.js
+++ web/reactjs/react-users/src/utils/userList.js
@@ -8,12 +8,12 @@ import img6 from '../assets/images/profile/img6.png'
 const userList = [
     {id: 1, name: "John", userName: "@john", profile: ""},
     {id: 2, name: "Sam", userName: "@sam", profile: img2},
-    {id: 3, name: "Alexa", userName: "@alexa", profile: img3},
-    {id: 4, name: "Kathrine", userName: "@kat", profile: ""},
+    {id: 3, name: "Alexa", userName: "@alexa", profile: img6},
+    {id: 4, name: "Kathrine", userName: "@kat", profile: img1},
     {id: 5, name: "Chips", userName: "@chips", profile: img4},
     {id: 6, name: "Martha", userName: "@martha", profile: img5},
     {id: 7, name: "Gi hun", userName: "@gi_hun", profile: ""},
-    {id: 8, name: "Samith", userName: "@samith", profile: img6},
+    {id: 8, name: "Samith", userName: "@samith", profile: img3},
 ];
 
 export default userList;
