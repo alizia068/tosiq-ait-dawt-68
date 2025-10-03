1d5daf6fb90d6edeb9f010c53220bd92e86da5aa
Muhammad Talha <mtalha0018@gmail.com>
2025-10-03 19:35:32 +0500

    React: Multipage link update

--- web/reactjs/react-multipage/src/components/Navbar.jsx
+++ web/reactjs/react-multipage/src/components/Navbar.jsx
@@ -2,16 +2,30 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 const Navbar = () => {
+
+  const links = [
+    { url: "/",  title: "Home"},
+    { url: "/about",  title: "About"},
+    { url: "/blog",  title: "Blog"},
+    { url: "/help",  title: "Help"},
+  ];
+
   return (
     <>
     <div className='m-5'>
       <nav className='flex items-center justify-between'>
         <div className='bg-white p-4 rounded-lg'><NavLink to={'/'}>Logo</NavLink></div>
-        <ul className='text-purple-900 flex gap-4 bg-white p-4 rounded-lg'>
-          <li className='hover:text-purple-500'><NavLink to={'/'}>Home</NavLink></li>
-          <li className='hover:text-purple-500'><NavLink to={'/about'}>About</NavLink></li>
-          <li className='hover:text-purple-500'><NavLink to={'/blog'}>Blog</NavLink></li>
-          <li className='hover:text-purple-500'><NavLink to={'/help'}>Help</NavLink></li>
+        <ul className='text-amber-500 flex gap-4 bg-white p-4 rounded-lg'>
+          { links.map((link, i)=>{
+            return (
+              <li key={i}>
+                <NavLink className={(e) => e.isActive ? "text-purple-600" : "hover:text-purple-500" } 
+                  to={link.url}>
+                    {link.title}
+                </NavLink>
+              </li>
+            )
+          }) }
         </ul>
       </nav>
     </div>
