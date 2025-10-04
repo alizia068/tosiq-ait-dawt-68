a75e24d2c06857fa36df1bad7e2fbc26fb1a8cab
Muhammad Talha <mtalha0018@gmail.com>
2025-10-04 20:01:27 +0500

    Team member detail page

--- web/reactjs/react-multipage-dynamic-routes/src/pages/TeamDetails.jsx
+++ web/reactjs/react-multipage-dynamic-routes/src/pages/TeamDetails.jsx
@@ -1,63 +1,76 @@
-import React, { useState, useEffect } from 'react'
-import Navbar from '../components/Navbar'
-import { useParams } from 'react-router-dom'
-import noImg from '../assets/no-img.png'
-import { GrGlobe, GrMapLocation } from 'react-icons/gr'
-import { ALL_USER_API } from '../resources/api'
+import React, { useState, useEffect } from "react";
+import Navbar from "../components/Navbar";
+import { useParams } from "react-router-dom";
+import noImg from "../assets/no-img.png";
+import { GrGlobe, GrMail, GrMapLocation, GrPhone } from "react-icons/gr";
+import { ALL_USER_API } from "../resources/api";
 
 const TeamDetails = () => {
-    const [teamMember, setTeam] = useState({})
-    const [loading, setLoading] = useState(true)
-    const params = useParams();
-    const id = params.id;
+  const [teamMember, setTeam] = useState({});
+  const [loading, setLoading] = useState(true);
+  const params = useParams();
+  const id = params.id;
 
-    useEffect(() => {
-        const getTeam = async () => {
-          const response = await fetch(ALL_USER_API + id)
-          const member = await response.json();
-          if (!member) {
-            console.log("No team member found")
-          } else {
-            setTeam(member)
-            setLoading(false)
-            console.log("data: "+ teamMember)
-          }
-        }
-    
-        getTeam()
-      })
+  useEffect(() => {
+    const getTeam = async () => {
+      const response = await fetch(ALL_USER_API + id);
+      const member = await response.json();
+      if (!member) {
+        console.log("No team member found");
+      } else {
+        setTeam(member);
+        setLoading(false);
+      }
+    };
 
-    return (
-        <div>
-        <Navbar />
-        <div className='p-5 bg-white/50 m-5 rounded-lg'>
-            <div className="rounded-lg bg-white p-3 w-[400px]">
-                <div className="flex items-start ">
+    getTeam();
+  });
+
+  return (
+    <div>
+      <Navbar />
+      <div className="p-5 bg-white/50 m-5 rounded-lg">
+        <div className="rounded-lg bg-white p-3">
+          <div className="flex justify-center">
+            <img src={noImg} className="rounded-full h-36 w-36" alt="" />
+          </div>
+          <div className="text-4xl text-center">{teamMember.name}</div>
+          <div className="lowercase text-purple-500 text-xl text-center">
+            @{teamMember.username}
+          </div>
+        </div>
+        <div className="gap-4 flex items-start justify-between">
+          <div className="w-full">
+            <div className="mt-4">Personal</div>
+            <div className="rounded-lg bg-white p-3">
+              <div className="text-sm text-gray-500">
                 <div>
-                    <img src={noImg} className="h-28 w-36 object-cover rounded-lg" />
+                   <GrMail className="inline -mt-1 mr-3" />{" "} {teamMember.email}
                 </div>
-                <div className="ml-3 w-full ">
-                    <div className="flex justify-between">
-                    <div>
-                        <span className="block line-clamp-1">{teamMember.name}</span>
-                        <small className="block mt-1">{teamMember.email}</small>
-                    </div>
-                    <div className="text-gray-400 text-[11px]">@{teamMember.username}</div>
-                    </div>
-                    <div className="mt-6 text-sm text-gray-500">
-                    <div>
-                        <GrMapLocation className="inline -mt-1 mr-3"/> {Object.entries(teamMember.address.city)}
-                    </div>
-                    <div>
-                        <GrGlobe className="inline -mt-1 mr-3"/> {teamMember.website}
-                    </div>
-                    </div>
+                <div className="">
+                  <GrPhone className="inline -mt-1 mr-3" />{" "} @{teamMember.phone}
                 </div>
-                </div>
+              </div>
             </div>
+          </div>
+          <div className="w-full">
+            <div className="mt-4">Business</div>
+            <div className="rounded-lg bg-white p-3">
+              <div className="text-sm text-gray-500">
+                <div>
+                  <GrMapLocation className="inline -mt-1 mr-3" />{" "}
+                  {teamMember.address?.city}
+                </div>
+                <div>
+                  <GrGlobe className="inline -mt-1 mr-3" /> {teamMember.website}
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default TeamDetails
+export default TeamDetails;
